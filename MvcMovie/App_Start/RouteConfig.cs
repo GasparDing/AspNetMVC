d5eb7a63ad400dd00db642f5eb9bdf8a2e87985a
GasparDing <kai21413@gmail.com>
2018-02-26 23:33:03 +0800

    first commit

---
+++ MvcMovie/App_Start/RouteConfig.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace MvcMovie
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            // When we don't supply any URL segments when application run.
+            // it defaults to "Home" controller and the "Index" action method
+            // because of the the defaults routing path below.
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+
+            // we can add custom routing rule
+            routes.MapRoute(
+                name: "Hello",
+                url: "{controller}/{action}/{name}/{num}"
+            );
+        }
+    }
+}
---
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // The following two aciton will reture string 
+        // you can specific the type you want to return.
+        // GET: HelloWorld
+        //public string Index()
+        //{
+        //    return "This is my <b>default</b> action";
+        //}
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: /HelloWorld/Welcome/
+        public string Welcome(string name, int num = 1)
+        {
+            // Security Note: The code above uses HttpUtility.HtmlEncode
+            // to protect the application from malicious input (namely JavaScript)
+            return HttpUtility.HtmlEncode("Hello " + name + ",num is " + num);
+        }
+    }
+}
